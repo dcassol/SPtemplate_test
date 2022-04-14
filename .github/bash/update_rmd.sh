0545d72540a09bbfbd4cad80019edcbfdfe5c756
dcassol <danicassol@gmail.com>
2022-04-13 21:19:40 -0700

    test

--- .github/bash/update_rmd.sh
+++ .github/bash/update_rmd.sh
@@ -26,16 +26,15 @@ mkdir -p ${update_dir}
 echo "Update files"
 cp -r ${down_folder}/vignettes/systemPipeRIBOseq.Rmd ${update_dir}
 cp -r ${down_folder}/vignettes/bibtex.bib ${update_dir}
-cp -r ${down_folder}/vignettes/results/* ${update_dir}/results/
+cp -r ${down_folder}/vignettes/results/ ${update_dir}/results/
 echo "done"
 
 # copy to inst
 echo "Update inst files"
 cp -r ${down_folder}/vignettes/systemPipeRIBOseq.Rmd inst/extdata/workflows/riboseq/
 cp -r ${down_folder}/vignettes/bibtex.bib inst/extdata/workflows/riboseq/
-cp -r ${down_folder}/vignettes/results/* inst/extdata/workflows/riboseq/results/
+cp -r ${down_folder}/vignettes/results/ inst/extdata/workflows/riboseq/results/
 cd inst/extdata/workflows/riboseq/
 Rscript -e "rmarkdown::render('systemPipeRIBOseq.Rmd', c('BiocStyle::html_document'), clean=T); knitr::knit('systemPipeRIBOseq.Rmd', tangle=TRUE)"
-rm -rf *cache/
 echo "done"
 
